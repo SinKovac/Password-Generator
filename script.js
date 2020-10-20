13b09e762d663c379a213a7b64b72b084db91c2a
SinKovac <71274524+SinKovac@users.noreply.github.com>
2020-10-20 15:34:31 -0400

    Updated javascrpt
    added in selectors and character codes

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-// Assignment Code
+// variables representing selectors
 var generateBtn = document.querySelector("#generate");
+var passwordText = document.querySelector("#password");
+var copyBtn = document.querySelector("#copy");
+
+// character codes that will make the base for the generated password
+let lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+let uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let numChars = "0123456789";
+let special = " !@#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
 
 // Write password to the #password input
 function writePassword() {
